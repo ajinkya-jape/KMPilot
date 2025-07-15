2c2a0f9778a248e7afc81411eaf3bdcf14d477c6
Ajinkya <japeajinkay@gmail.com>
2025-07-15 17:28:24 +0530

    Initial commit

---
+++ shared/src/commonMain/kotlin/dev/ajinkyajape/kmpilot/Greeting.kt
@@ -0,0 +1,9 @@
+package dev.ajinkyajape.kmpilot
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
